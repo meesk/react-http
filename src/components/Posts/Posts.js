5c2ac22283ff8aadae3aefef81e5ec51a36e79bc
Mees Kluivers <meeskluivers@gmail.com>
2018-04-05 14:40:09 +0200

    cleanup

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Post from './Post/Post';
 
-const posts = (props) => props.posts.map((post, index) => {
+const posts = (props) => props.posts.map( post => {
     return <Post 
     title={post.title}
     body={post.body}
